081ad9ef602e36b91d77c77467056932ac5539c8
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2025-03-03 15:48:55 +0100

    Normalize line endings

---
+++ my-helper/src/app/page.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+// // import styles from "./page.module.css";
+// import { FC } from "react";
+
+// export const Homepage:FC =()=>{
+//   return(
+//     <div>Home</div>
+//   )
+// }
